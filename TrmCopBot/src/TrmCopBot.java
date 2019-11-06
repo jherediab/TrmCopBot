59332f34ae2ae85d64b090db0b4e2968c9eda19c
jherediab <jherediab@gmail.com>
2019-11-06 18:54:40 -0500

    6-nov-2019 -- clase main adicionada a la clase TrmCopBot

--- TrmCopBot/src/TrmCopBot.java
+++ TrmCopBot/src/TrmCopBot.java
@@ -3,6 +3,8 @@ import java.rmi.RemoteException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -117,4 +119,23 @@ public class TrmCopBot extends TelegramLongPollingBot{
 		return "1056358363:AAGeHgh2GonqYfTKxumVE_nSVuaGTiXjVQE";
 	}
 
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new TrmCopBot());
+           
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    
+
+	}
+	
 }
